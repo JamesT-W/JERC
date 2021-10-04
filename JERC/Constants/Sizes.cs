b4885e5bb77aad7aa65d84fa8b25e357b8b9f49f
Jimmy T-W <jimmywoodhouse1@btinternet.com>
2021-10-04 09:54:39 +0100

    minor

--- JERC/Constants/Sizes.cs
+++ JERC/Constants/Sizes.cs
@@ -8,6 +8,6 @@ namespace JERC.Constants
     {
         public static readonly float MaxHammerGridSize = 32768.000000f;
         public static readonly int FinalOutputImageResolution = 1024;
-        public static readonly int SizeReductionMultiplier = 1; // this should use scale from the overview txt
+        public static readonly float SizeReductionMultiplier = 1.0f; // this is not taken into account currently, values other than 1.0 will make the overview image offset and not aligned in game either
     }
 }
